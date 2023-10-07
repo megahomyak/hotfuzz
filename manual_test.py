86b85d82d330ddc9d0d8a6fd6be748849f8cb643
megahomyak <g.megahomyak@gmail.com>
2023-10-07 15:48:20 +0500

    Expanded the items list

--- manual_test.py
+++ manual_test.py
@@ -1,4 +1,9 @@
 import hotfuzz
 
-hotfuzz = hotfuzz.HotFuzz(["abc", "Abc", "BAB", "01234567890123456789012345678901234567890123456789"])
+items = ["abc", "Abc", "BAB", "01234567890123456789012345678901234567890123456789"]
+
+for i in range(100):
+    items.append("o" + str(i))
+
+hotfuzz = hotfuzz.HotFuzz(items)
 print(hotfuzz.run())
